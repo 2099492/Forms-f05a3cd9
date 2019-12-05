357d1ee38d7be443f552920b3dda19aeb40f817d
2099492 <2099492@talnet.com>
2019-12-05 14:47:16 +0100

    Uitwerking van de exercise

---
+++ form.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <form class="hallo" action="" method="get">
+    <div class="hallo">
+      <input type="text" name="name" placeholder="voer je e-mailadres in">
+
+
+       <div>
+
+    </div>
+
+    </div>
+
+    </form>
+    <?php
+
+     ?>
+  </body>
+</html>
